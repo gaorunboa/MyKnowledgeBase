4b0a386eec14a3811c75a7cee594e5ca764e0090
gaorunbo <1975880301@qq.com>
2026-04-23 08:58:11 +0800

    init: Obsidian 知识库初始化 3

--- 140_git_add_and_push.bat
+++ 140_git_add_and_push.bat
@@ -1,5 +1,6 @@
+chcp 65001 >nul
 git add .
-git commit -m "init: Obsidian 知识库初始化 2"
+git commit -m "init: Obsidian 知识库初始化 3"
 git remote add origin https://github.com/gaorunboa/MyKnowledgeBase
 git push -u origin master
 timeout 30
